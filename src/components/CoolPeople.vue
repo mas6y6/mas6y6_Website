205f3e5f7a41916f3831cc31734d730a460e7f9d
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2026-07-29 18:07:51 -0500

    techmoblogiblabyte

--- src/components/CoolPeople.vue
+++ src/components/CoolPeople.vue
@@ -9,33 +9,38 @@ const openLink = (url) => {
     <h1>Cool people</h1>
     <p>These people have pushed me so far to reach this point. I appreciate them and wouldn't be here without them.</p>
     <div class="people-links">
+      <div class="people" id="people-tech" @click="openLink('https://technologicalbyte.com/')">
+        <img src="/assets/people/tech.webp" alt="TechnologicalByte" />
+        <h2>TechnologicalByte</h2>
+      </div>
+
       <div class="people" id="people-batista" @click="openLink('https://batista.parafieldstudios.com/')">
-        <img src="/assets/people/batista.jpg" alt="Discord" />
+        <img src="/assets/people/batista.jpg" alt="Batista" />
         <h2>Batista</h2>
       </div>
 
       <div class="people" id="people-st6" @click="openLink('https://technologicalshadows-home.carrd.co/')">
-        <img src="/assets/people/st6.png" alt="Discord" />
+        <img src="/assets/people/st6.png" alt="EclipseTech" />
         <h2>EclipseTech4820</h2>
       </div>
 
       <div class="people" id="people-malachi196" @click="openLink('https://discordapp.com/users/1166148454603571280')">
-        <img src="/assets/people/malachi196.png" alt="Discord" />
+        <img src="/assets/people/malachi196.png" alt="Malach" />
         <h2>malachi196</h2>
       </div>
 
       <div class="people" id="people-mushy" @click="openLink('https://discordapp.com/users/1016878311005225032')">
-        <img src="/assets/people/mushy.png" alt="Discord" />
+        <img src="/assets/people/mushy.png" alt="Mushy" />
         <h2>Mushy</h2>
       </div>
 
       <div class="people" id="people-notdef" @click="openLink('https://discordapp.com/users/1278494594560032849')">
-        <img src="/assets/people/notdef.png" alt="Discord" />
+        <img src="/assets/people/notdef.png" alt="notdefyingphysics" />
         <h2>notdefyingphysics</h2>
       </div>
 
       <div class="people" id="people-kebin" @click="openLink('https://github.com/shadowmewtwogx2020')">
-        <img src="/assets/people/kebin.png" alt="Discord" />
+        <img src="/assets/people/kebin.png" alt="WavierGolem5727" />
         <h2>WavierGolem5727</h2>
       </div>
     </div>
